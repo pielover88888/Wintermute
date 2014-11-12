8f4115b983c5245b18f4aee4058333a0fb6e87a2
Edwin Pers <epers@primesli.me>
2014-11-12 14:09:31 +0000

    lolnope

--- wintermute.js
+++ wintermute.js
@@ -7,7 +7,7 @@ GLOBAL.fs = require('fs')
 
 var config = {
     botName: 'Wintermute',
-    channels: ['#wetfish', '#testing'],
+    channel: '#wetfish',
     commandChar: '.',
     master: 'Weazzy',
     moduleDir: './modules/',
@@ -16,7 +16,7 @@ var config = {
 
 GLOBAL.bot = new irc.Client(config.server, config.botName, 
 			    {
-				channels: config.channels, 
+				channels: [config.channel], 
 				userName: config.botName, 
 				realName: config.botName, 
 				secure: true,

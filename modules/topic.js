e6995d5f26cd926513bf58a4421859224429f777
Edwin Pers <epers@primesli.me>
2014-11-12 08:59:58 +0000

    oh, right

--- modules/topic.js
+++ modules/topic.js
@@ -3,7 +3,7 @@
 bot.modules.topic = function(from, to, text, message) {
   getTitle(function(word1) {
     getTitle(function(word2) {
-      bot.send("TOPIC", bot.config.channel, 'Soupwhale: The ' + word1 + ' to your ' + word2);
+      bot.send("TOPIC", bot.config.channel, 'Wetfish: The ' + word1 + ' to your ' + word2);
     })
   })
 }

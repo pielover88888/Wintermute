5e66e8e7ab26dd73cba829b5eb1ec5c24b17b3d7
Edwin Pers <pers.edwin@gmail.com>
2014-11-13 05:02:34 -0500

    pls work

--- modules/clvr.js
+++ modules/clvr.js
@@ -1,28 +1,31 @@
-var CleverBot = new require('cleverbot-node')
-  , clever = new CleverBot()
-  , protection = require('../inc/clvr/echo_protection')
-  , maybeSpiceUp = require('../inc/clvr/fullmoon_spiceup');
 
-const ignoreMax = 3600;
-
-var insult = (function() {
-  var insults = [
-    '.',
-    '..',
-    'Get lost.',
-    '...',
-    'You should be working.',
-    'This is not a productive area of discussion.',
-    'Do you even lift?'
-  ];
-  var insIdx = -1;
-  return function() {
-    insIdx = (insIdx + 1) % insults.length;
-    return insults[insIdx];
-  };
-}());
 
 bot.modules.clvr = function(from, to, text, message) {
+
+  var CleverBot = new require('cleverbot-node')
+    , clever = new CleverBot()
+    , protection = require('../inc/clvr/echo_protection')
+    , maybeSpiceUp = require('../inc/clvr/fullmoon_spiceup');
+
+  const ignoreMax = 3600;
+
+  var insult = (function() {
+    var insults = [
+      '.',
+      '..',
+      'Get lost.',
+      '...',
+      'You should be working.',
+      'This is not a productive area of discussion.',
+      'Do you even lift?'
+    ];
+    var insIdx = -1;
+    return function() {
+      insIdx = (insIdx + 1) % insults.length;
+      return insults[insIdx];
+    };
+  }());
+
   if(!protection.isIgnored(from)) {
     if(protection.isTooSimilar(from, text)) {
       protection.ignore(from, ignoreMax);

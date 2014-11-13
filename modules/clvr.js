8b30664d67ac0911833380e7b8386b8c29b561f5
Edwin Pers <pers.edwin@gmail.com>
2014-11-13 04:55:20 -0500

    Implemented cleverbot functionality from https://github.com/clux/cleverbot-irc

---
+++ modules/clvr.js
@@ -0,0 +1,39 @@
+var CleverBot = new require('cleverbot-node')
+  , clever = new CleverBot()
+  , protection = require('../inc/clvr/echo_protection')
+  , maybeSpiceUp = require('../inc/clvr/fullmoon_spiceup');
+
+const ignoreMax = 3600;
+
+var insult = (function() {
+  var insults = [
+    '.',
+    '..',
+    'Get lost.',
+    '...',
+    'You should be working.',
+    'This is not a productive area of discussion.',
+    'Do you even lift?'
+  ];
+  var insIdx = -1;
+  return function() {
+    insIdx = (insIdx + 1) % insults.length;
+    return insults[insIdx];
+  };
+}());
+
+bot.modules.clvr = function(from, to, text, message) {
+  if(!protection.isIgnored(from)) {
+    if(protection.isTooSimilar(from, text)) {
+      protection.ignore(from, ignoreMax);
+      bot.speak(insult());
+    }
+  }
+  else {
+    clever.write(text, function(data) {
+      var resp = data.message;
+      protection.remember(from, resp);
+      bot.speak(maybeSpiceUp(resp));
+    });
+  }
+};

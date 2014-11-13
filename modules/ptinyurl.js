997b7c27fcff9b0824acbe0280262bc702db4c9b
Edwin Pers <epers@primesli.me>
2014-11-13 21:18:52 +0000

    added private tinyurl module

---
+++ modules/ptinyurl.js
@@ -0,0 +1,17 @@
+//Converts long links into tinyurls
+
+bot.modules.line.tinyurl = function(from, to, text, message) {
+  if(text.search(/\bhttps?:\/\/.*?\..*?\b/) !== -1) {
+    var url = '' + text.match(/http\S*/);
+    if(url.length > 64) {
+      request('http://tinyurl.com/api-create.php?url=' + url, function(e, r, body) {
+        if (body != "Error" && body.length < 30) {
+	    bot.speak_admin("Link from" + from + " saying " + text);
+          bot.speak_admin(irc.colors.codes.orange + body.replace(/^http/, 'https'));
+        } else {
+          bot.speak_admin("\u000304I am error!");
+        }
+      });
+    }
+  }
+};

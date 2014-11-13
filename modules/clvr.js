b48ce0b5fc2e9f5ecfe115b544ac47fffb47c1d7
Edwin Pers <pers.edwin@gmail.com>
2014-11-13 05:25:50 -0500

    fuck

--- modules/clvr.js
+++ modules/clvr.js
@@ -1,13 +1,11 @@
 bot.modules.clvr = function(from, to, text, message) {
 
   bot.speak_test("a");
-  var CleverBot = new require('cleverbot-node')
+  var CleverBot = new require('cleverbot-node'
     , clever = new CleverBot()
 
     clever.write(text, function(data) {
       bot.speak_test("d");
-      var resp = data.message;
-      protection.remember(from, resp);
-      bot.speak_test(resp);
+      bot.speak_test(data.message);
     });
 };

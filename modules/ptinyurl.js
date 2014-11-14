b017de3fd3729d84d209593a524c582393372e29
Edwin Pers <epers@primesli.me>
2014-11-14 03:29:22 +0000

    fixed a little thing

--- modules/ptinyurl.js
+++ modules/ptinyurl.js
@@ -6,7 +6,8 @@ bot.modules.line.tinyurl = function(from, to, text, message) {
     if(url.length > 64) {
       request('http://tinyurl.com/api-create.php?url=' + url, function(e, r, body) {
         if (body != "Error" && body.length < 30) {
-	    bot.speak_admin("Link from" + from + " saying " + text);
+	    //bot.speak_admin("From: " + from + " To: " + to + " Text: " + text + " Message: " + message);
+	    bot.speak_admin("Link from " + from + " saying " + text);
           bot.speak_admin(irc.colors.codes.orange + body.replace(/^http/, 'https'));
         } else {
           bot.speak_admin("\u000304I am error!");

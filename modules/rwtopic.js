451ffeca877a4708ac8b53f443d4ded72b4bfe64
Edwin Pers <epers@primesli.me>
2014-11-21 10:26:35 +0000

    made .rwtopic play nice with :topic

--- modules/rwtopic.js
+++ modules/rwtopic.js
@@ -3,7 +3,9 @@
 bot.modules.rwtopic = function(from, to, text, message) {
   getTitle(function(word1) {
     getTitle(function(word2) {
-      bot.send("TOPIC", bot.config.channel, 'Wetfish: The ' + word1 + ' to your ' + word2);
+	var c = require('irc-colors');
+	bot.speak2(to, ":topic set " + c.bold("#wetfish") + " | The " + c.bold(word1) + " to your " + c.bold(word2)); 
+	//bot.send("TOPIC", bot.config.channel, 'Wetfish: The ' + word1 + ' to your ' + word2);
     })
   })
 }

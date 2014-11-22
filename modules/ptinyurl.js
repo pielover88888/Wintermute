5bbb71c08349713c36f382ba5b61c925304b169b
Edwin Pers <epers@primesli.me>
2014-11-22 08:07:59 +0000

    cleaned up some stuff

--- modules/ptinyurl.js
+++ modules/ptinyurl.js
@@ -7,7 +7,7 @@ bot.modules.line.tinyurl = function(from, to, text, message) {
       request('http://tinyurl.com/api-create.php?url=' + url, function(e, r, body) {
         if (body != "Error" && body.length < 30) {
 	    //bot.speak_admin("From: " + from + " To: " + to + " Text: " + text + " Message: " + message);
-	    bot.speak_admin("Link from " + from + " saying " + text);
+	    bot.speak2("#wintermute", "Link from " + from + " saying " + text);
           bot.speak_admin(irc.colors.codes.orange + body.replace(/^http/, 'https'));
         } else {
           bot.speak_admin("\u000304I am error!");
--- modules/tinyurl.js
+++ modules/tinyurl.js
@@ -3,7 +3,7 @@
 bot.modules.line.tinyurl = function(from, to, text, message) {
   if(text.search(/\bhttps?:\/\/.*?\..*?\b/) !== -1) {
     var url = '' + text.match(/http\S*/);
-    if(url.length > 64) {
+    if(url.length > 100) {
       request('http://tinyurl.com/api-create.php?url=' + url, function(e, r, body) {
         if (body != "Error" && body.length < 30) {
           bot.speak(irc.colors.codes.orange + body.replace(/^http/, 'https'));
